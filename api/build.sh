b086991f46df54eebda7bfb6161f6906d5ae94c9
DeSutterWout <desutterwout6@gmail.com>
2025-05-24 16:38:34 +0200

    edit requirements

--- api/build.sh
+++ api/build.sh
@@ -11,5 +11,8 @@ pip install wheel
 # Install mysql-connector-python explicitly first
 pip install mysql-connector-python==8.3.0
 
+# Install APScheduler explicitly
+pip install APScheduler==3.10.4
+
 # Install other dependencies
 pip install -r requirements.txt 
